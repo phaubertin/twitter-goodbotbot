691d378630f691fbd3d8c74e3efc4d2c3c6cf541
Philippe Aubertin <philippe.aubertin@gmail.com>
2020-04-26 23:45:30 -0400

    index.py: code complete

--- index.py
+++ index.py
@@ -26,28 +26,38 @@
 from __future__ import absolute_import, division, print_function, unicode_literals
 
 import os
+import random
 import sys
 import tweepy
 
-# Patch supported libraries to instrument them with AWS X-Ray. In our case, this
-# means the requests library used by Tweepy.
-#
-# Both the patching and the import are done conditionally to ensure there is a
-# dependency on the AWS X-Ray SDK only when the code runs on AWS Lambda. We use
-# the presence of the AWS_EXECUTION_ENV environment variable to detect that the
-# code is running in AWS Lambda.
-#
-# Performance note: previously, we did this in the AWS Lambda handler function
-# (i.e. the handler() function) instead of here. This was adding 5.5 seconds to
-# each invocation.
+from datetime import datetime, timezone
+
+# Seed random number generator during initialization.
+random.seed()
+
+# We use the presence of the AWS_EXECUTION_ENV environment variable to detect
+# that the code is running in AWS Lambda.
 if os.environ.get('AWS_EXECUTION_ENV', None):
+    # Import boto3 package needed to invoke the Step Function state machine
+    # from the AWS Lambda function.
+    import boto3
+    
+    # Patch supported libraries to instrument them with AWS X-Ray. In our case,
+    # this means the requests library used by Tweepy.
+    #
+    # Both the patching and the import are done conditionally to ensure there
+    # is a dependency on the AWS X-Ray SDK only when the code runs on AWS Lambda. 
+    #
+    # Performance note: previously, we did this in the AWS Lambda handler function
+    # (i.e. the handler() function) instead of here. This was adding 5.5 seconds
+    # to each invocation.
     from aws_xray_sdk.core import patch_all
     patch_all()
 
 # Default maximum age of a tweet to which the bot will reply, in minutes.
 DEFAULT_MAXAGE = 6 * 60
 
-MESSAGES = [    
+MESSAGES = frozenset([
     'Admirable bot.',
     'Amazing bot.',
     'Awesome bot.',
@@ -87,8 +97,8 @@ MESSAGES = [
     'I appreciate your work.',
     'I am grateful for your work.',
     'Well done.',
-    'Very well done.'
-]
+    'Continue your great work.'
+])
 
 class GoodBotBotException(Exception):
     """Base class for exceptions thrown by this application"""
@@ -97,6 +107,18 @@ class GoodBotBotException(Exception):
 
 class ConfigurationException(GoodBotBotException):
     """Exception that indicates a configuration error such as a missing configuration option."""
+    
+    pass
+    
+class NoTimelineException(GoodBotBotException):
+    """Exception that indicates the user timeline of the controlled Twitter account is empty.
+    
+    Even though this might simply be because the account never tweeted, out of
+    an abundance of caution, this bot will not tweet in this case. We want to
+    be sure the bot can see the replies when checking whether it already replied
+    to a specific tweet, because we don't want the bot to reply repeatedly to
+    the same tweet.
+    """
     
     pass
 
@@ -128,31 +150,39 @@ def load_configuration(config=None, environ=None):
     config_options  = [
         {
             'key'       : 'app_key',
-            'environ'   : 'BOT_APP_KEY'
+            'environ'   : 'BOT_APP_KEY',
+            'is_int'    : False
+                
         },
         {
             'key'       : 'app_secret',
-            'environ'   : 'BOT_APP_SECRET'
+            'environ'   : 'BOT_APP_SECRET',
+            'is_int'    : False
         },
         {
             'key'       : 'access_token',
-            'environ'   : 'BOT_ACCESS_TOKEN'
+            'environ'   : 'BOT_ACCESS_TOKEN',
+            'is_int'    : False
         },
         {
             'key'       : 'token_secret',
-            'environ'   : 'BOT_TOKEN_SECRET'
+            'environ'   : 'BOT_TOKEN_SECRET',
+            'is_int'    : False
         },
         {
             'key'       : 'target',
-            'environ'   : 'BOT_TARGET'
+            'environ'   : 'BOT_TARGET',
+            'is_int'    : False
         },
         {
             'key'       : 'source',
-            'environ'   : 'BOT_SOURCE'
+            'environ'   : 'BOT_SOURCE',
+            'is_int'    : False
         },
         {
             'key'       : 'maxage',
-            'environ'   : 'BOT_MAXAGE'
+            'environ'   : 'BOT_MAXAGE',
+            'is_int'    : True
         }
     ]
     
@@ -160,8 +190,11 @@ def load_configuration(config=None, environ=None):
     for option in config_options:
         value = environ.get(option['environ'], None)
         
-        if value:
-            config[option['key']] = value
+        if value is not None:
+            if option['is_int']:
+                config[option['key']] = int(value)
+            else:
+                config[option['key']] = value
     
     # Set default values
     set_defaults(config)
@@ -185,6 +218,41 @@ def instanciate_api(config):
     
     return tweepy.API(auth)
 
+def pretty_duration(seconds):
+    """Return a human-readable string for the specified duration"""
+    
+    if seconds < 2:
+        return '%d second' % seconds
+    elif seconds < 120:
+        return '%d seconds' % seconds
+    elif seconds < 7200:
+        return '%d minutes' % (seconds // 60)
+    elif seconds < 48 * 3600:
+        return '%d hours' % (seconds // 3600)
+    else:
+        return '%d days' % (seconds // (24 * 3600))
+
+def print_rate_limit(response):
+    """Retrieve rate limit remaining from response headers and print it."""
+    
+    reset_header = response.headers.get('x-rate-limit-reset', None)
+    
+    if reset_header is None:
+        reset = '??'
+    else:
+        try:
+            reset_time      = datetime.fromtimestamp(int(reset_header), tz=timezone.utc)
+            reset_in        = reset_time - datetime.now(timezone.utc)
+            reset           = pretty_duration(reset_in.total_seconds())
+        except:
+            reset       = '?!?'
+    
+    print('Rate limit remaining %s/%s resets in %s' % (
+        response.headers.get('x-rate-limit-remaining', '??'),
+        response.headers.get('x-rate-limit-limit',     '??'),
+        reset
+    ))
+
 def get_latest_candidate_tweet(api, config):
     """Find the latest candidate tweet.
     
@@ -200,6 +268,7 @@ def get_latest_candidate_tweet(api, config):
     """
     
     tweets = api.user_timeline(screen_name=config['target'], count=20)
+    print_rate_limit(api.last_response)
     
     for tweet in tweets:
         if tweet.author.screen_name != config['target']:
@@ -218,20 +287,67 @@ def get_latest_candidate_tweet(api, config):
     
     # No candidate tweet.
     return None
-
-def get_tweet(api, config):
-    """Get tweet to which to reply"""
+        
+def choose_reply(api, tweet):
+    """Choose a reply for the specified tweet if it hasn't been replied to.
     
-    candidate = get_latest_candidate_tweet(api, config)
+    Checks the last 20 tweets by the controlled account for a reply to the
+    specified tweet. If the tweet has already been replied to, return None.
+    Otherwise, return a reply for it.
+    """
     
-    print_tweet(candidate)
+    mytweets = api.user_timeline(count=20)
     
-    if not candidate:
-        return None
+    if len(mytweets) < 1:
+        # The user timeline is empty.
+        #
+        # Even though this might simply be because the account never tweeted,
+        # out of an abundance of caution, this bot will not tweet in this case.
+        # We want to be sure the bot can see the replies because we don't want
+        # the bot to reply repeatedly to a tweet.
+        raise NoTimelineException("Controlled account's timeline is empty")
+    
+    messages = set(MESSAGES)
+    
+    for mytweet in mytweets:
+        # Check if this is a reply to the target tweet.
+        if mytweet.in_reply_to_status_id == tweet.id:
+            # This is an existing reply to the target tweet.
+            return None
+        
+        # Remove this reply from the set of candidate replies.
+        messages = set(x for x in messages if x not in tweet.text)
+    
+    # Return a random message from the remaining ones.
+    return random.sample(messages, 1)[0]
+    
+def time_since_tweet(tweet, now=None):
+    """Get the number of seconds that have elapsed since a tweet was created."""
+    
+    if now is None:
+        now = datetime.now(timezone.utc)
+    
+    # The date and time reported by the Twitter API is in the UTC time zone.
+    # However, the Tweepy library parses this into a "naive" datetime object
+    # without timezone information. Let's fix that.
+    tweet_time  = datetime.combine(
+                    tweet.created_at.date(),
+                    tweet.created_at.time(),
+                    tzinfo=timezone.utc)
+    
+    # Compute time elapsed since tweet.
+    delta       = now - tweet_time
+    seconds     = delta.total_seconds()
+    
+    if seconds < 0:
+        # Clock skew???
+        return 0
     else:
-        return tweet    
-    
+        return seconds
+        
 def print_tweet(tweet):
+    """Print some information about a tweet."""
+    
     if not tweet:
         print('No tweet')
     else:
@@ -240,22 +356,109 @@ def print_tweet(tweet):
         print('.  Author:   ' + tweet.author.screen_name)
         print('.  ' + tweet.text)
 
+def choose_candidate_tweet(api, config):
+    """Get tweet to which to reply"""
+    
+    candidate = get_latest_candidate_tweet(api, config)
+    
+    print_tweet(candidate)
+    
+    if candidate is None:
+        return None
+    
+    seconds_since = time_since_tweet(candidate)
+    
+    # Configuration value is in minutes. We convert all time intervals to
+    # seconds before comparing and printing.
+    maxage = config['maxage'] * 60
+    
+    print("Tweet was posted %s ago." % pretty_duration(seconds_since))
+    
+    if seconds_since > maxage:
+        print("Tweet was posted more than %s ago." % pretty_duration(maxage))
+        return None
+        
+    reply = choose_reply(api, candidate)
+    
+    if not reply:
+        print('Already replied to this tweet.')
+        return None
+    
+    return candidate
+
+def reply_to_tweet(api, tweet_id, dry_run):
+    """Reply to the specified tweet."""
+    
+    tweet = api.get_status( int(tweet_id) )
+    
+    print_tweet(tweet)
+    
+    reply = choose_reply(api, tweet)
+    
+    if not reply:
+        print('Already replied to this tweet.')
+    else:
+        print("Reply: " + reply)
+        
+        if dry_run:
+            print('Dry run - not sending reply')
+        else:
+            api.update_status(
+                '@%s %s' % (tweet.author.screen_name, reply),
+                in_reply_to_status_id = int(tweet_id)
+            )
+            
+            print('Reply tweet sent.')
+
+def run_state_machine(state_arn, tweet_id):
+    """Invoke Step Function state machine
+    
+    The state machines fullfills two goals:
+    1) It manages retries in case sending the reply fails.
+    2) We use the enforced unicity of the execution name to ensure the bot will
+       attempt to reply to a tweet only once, even if the Lambda function is
+       invoked multiple times concurrently.
+    """
+
+    client = boto3.client('stepfunctions')
+    
+    client.start_execution(
+        stateMachineArn = state_arn,
+        # Generating a name that contains the tweet ID ensures the state machine
+        # will be run only once for this tweet by the bot.
+        name            = 'GoodBotBot-tweet-id-' + tweet_id,
+        input           = json.dumps({
+            'tweet-id' : tweet_id
+        })
+    )
+
 def handler(event, context):
     """Aws Lambda Function handler."""
     
     config      = load_configuration()
     api         = instanciate_api(config)
-    candidate   = get_latest_candidate_tweet(api, config)
     
-    print_tweet(candidate)
+    state_arn   = event.get('state-machine-arn',  None)
+    tweet_id    = event.get('tweet-id', None)
+    dry_run     = event.get('dry-run',  False)
+    
+    if tweet_id is None:
+        tweet = choose_candidate_tweet(api, config)
+        
+        if state_arn is None:
+            print("Warning: no Step Function state machine ARN provided")
+        elif tweet is not None:
+            run_state_machine(state_arn, tweet.id_str)
+    else:
+        print('Tweet ID argument: ' + tweet_id)        
+        reply_to_tweet(api, tweet_id, dry_run)
 
 def main(argv):
 
     config      = load_configuration()
     api         = instanciate_api(config)
-    candidate   = get_latest_candidate_tweet(api, config)
     
-    print_tweet(candidate)
+    choose_candidate_tweet(api, config)
 
 if __name__ == '__main__':
     main(sys.argv)
